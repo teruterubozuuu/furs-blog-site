ec16d8331c4c1317a10fc7eab092755713238b6a
Joylynne Esportuno <degozarunyan@gmail.com>
2025-07-24 13:58:58 +0800

    chore:fix user interface of sign in page

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -3,19 +3,20 @@
 @section('title', 'Sign In')
 
 @section('content')
-<div class="flex flex-wrap justify-center py-8  gap-8">
-   <div class="flex flex-col justify-center space-y-3 shadow-sm rounded-sm h-70 p-4 max-w-100 w-70 max-h-50 bg-white">
-        <form method="POST">
+<div class="flex flex-wrap justify-center p-10">
+    <div class="flex flex-col justify-center items-center max-w-200 w-100 space-y-3 shadow-sm rounded-sm p-5 bg-white">
+        <form method="POST" class="p-8 w-full space-y-5">
             @csrf
+            <h1 class="text-center font-bold text-[#2e7d32] text-xl">Sign in to your account</h1>
             <div class="mb-4">
                 <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <input type="email" name="email" id="email" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                <input type="email" name="email" id="email" required class="mt-1 p-1 block w-full rounded-sm bg-gray-100 shadow-sm focus:outline-none">
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input type="password" name="password" id="password" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                <input type="password" name="password" id="password" required class="mt-1 p-1 block w-full rounded-sm bg-gray-100 shadow-smfocus:outline-none">
             </div>
-            <button type="submit" class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700">Sign In</button>
+            <button type="submit" class="w-full bg-[#2e7d32] text-white py-2 px-4 rounded-md cursor-pointer transition-all ease-in hover:bg-[#356338]">Sign In</button>
         </form>
     </div>
 </div>
